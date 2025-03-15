b8a2ed473803039bb6fb4a8795b9cad5732fdd89
jme418 <jkingmues@gmail.com>
2025-03-15 12:15:15 +0100

    actualizado archivo provider.tf para comentar la variable de entorno GOOGLE_CLOUD_KEYFILE_JSON

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # Descargar json con credenciales de aquí:
 # https://console.cloud.google.com/apis/credentials/serviceaccountkey
 # Tras ello definir la variable de entorno apuntando a el json
-export GOOGLE_CLOUD_KEYFILE_JSON=./credentials/cnsa2025-jme418-6ee5b88247e8.json
+# export GOOGLE_CLOUD_KEYFILE_JSON=path/file.json
 
 variable "gcp_project" {
   # Configurar el nombre del proyecto en GCP
