0f3467b7d68abca2b6a3dc18a8f5ea96c2ec430a
jme418 <jkingmues@gmail.com>
2025-03-15 12:10:19 +0100

    actualizado archivo provider.tf con nuevas credenciales y versión de proveedor de Google Cloud

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
 # Descargar json con credenciales de aquí:
 # https://console.cloud.google.com/apis/credentials/serviceaccountkey
 # Tras ello definir la variable de entorno apuntando a el json
-# export GOOGLE_CLOUD_KEYFILE_JSON=path/file.json
+export GOOGLE_CLOUD_KEYFILE_JSON=./credentials/cnsa2025-jme418-6ee5b88247e8.json
 
 variable "gcp_project" {
   # Configurar el nombre del proyecto en GCP
-  default = "cnsa-2024"
+  default = "cnsa2025-jme418"
 }
 
 terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.18.0"
+      version = "6.25.0"
     }
   }
 }
